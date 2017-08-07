3727b95361c8ed59f4e42bdf157dc573715be72d
Michael Bielodied <mbeloded@gmail.com>
2017-08-07 08:27:01 +0300

    removed commented code

--- discounts/Classes/Managers/CategoryManager/CategoryManager.swift
+++ discounts/Classes/Managers/CategoryManager/CategoryManager.swift
@@ -13,26 +13,7 @@ class CategoryManager: DataManager {
     static let sharedInstance = CategoryManager()
     
     var categoryArrayData = Array<CategoryObject>()
-    /*
-    func loadJSONFromBundle(_ filename: String) -> [String: AnyObject]? {
-        
-        if let url = Bundle.main.url(forResource: filename, withExtension: "json")
-        {
-            if let data = NSData(contentsOf: url) {
-                do {
-                    let object = try JSONSerialization.jsonObject(with: data as Data, options: .allowFragments)
-                    if let dictionary = object as? [String: AnyObject] {
-                        return dictionary
-                    }
-                } catch {
-                    print("Error!! Unable to parse  \(filename).json")
-                }
-            }
-            print("Error!! Unable to load  \(filename).json")
-        }
-        return nil
-    }
-    */
+    
     func loadDataFromJSON(_ filename: String)
     {
         if let dictionary = self.loadJSONFromBundle(filename)
--- discounts/Classes/Managers/DiscountsManager/DiscountsManager.swift
+++ discounts/Classes/Managers/DiscountsManager/DiscountsManager.swift
@@ -14,26 +14,7 @@ class DiscountsManager: DataManager {
     
     var discountsArrayData = Array<CompanyObject>()
     var discountsCategoryArrayData = Array<CompanyObject>()
-/*
-    func loadJSONFromBundle(_ filename: String) -> [String: AnyObject]? {
-        
-        if let url = Bundle.main.url(forResource: filename, withExtension: "json")
-        {
-            if let data = NSData(contentsOf: url) {
-                do {
-                    let object = try JSONSerialization.jsonObject(with: data as Data, options: .allowFragments)
-                    if let dictionary = object as? [String: AnyObject] {
-                        return dictionary
-                    }
-                } catch {
-                    print("Error!! Unable to parse  \(filename).json")
-                }
-            }
-            print("Error!! Unable to load  \(filename).json")
-        }
-        return nil
-    }
-*/
+
     func loadDataFromJSON(_ filename: String)
     {
         if(discountsArrayData.count > 0)
